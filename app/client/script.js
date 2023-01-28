965316a06dbce6d355a39d69f25808aa8ab115b5
shawnWekesa <102484330+ShawnWekesa@users.noreply.github.com>
2023-01-28 18:47:58 +0300

    finished 2/3 of hero

---
+++ app/client/script.js
@@ -0,0 +1,23 @@
+console.log('hi');
+
+// const homeSection1 = document.querySelector('#home .job');
+// const homeSection2 = document.querySelector('#home .labour');
+// const homeSection3 = document.querySelector('#home .categories');
+
+// console.log(homeSection1)
+
+// homeSection1.addEventListener("mouseover", (e) =>{
+// 	e.target.style.width = "60vw";
+// 	homeSection2.target.style.width = "20vw";
+// 	homeSection3.target.style.width = "20vw";
+// });
+// homeSection2.addEventListener("mouseover", (e) =>{
+// 	e.target.style.width = "60vw";
+// 	homeSection1.target.style.width = "20vw";
+// 	homeSection3.target.style.width = "20vw";
+// });
+// homeSection3.addEventListener("mouseover", (e) =>{
+// 	e.target.style.width = "60vw";
+// 	homeSection1.target.style.width = "20vw";
+// 	homeSection2.target.style.width = "20vw";
+// });
