6bbdce5ab0a7cb71b6795bf616bb4f7a9b2beadf
shawnWekesa <102484330+ShawnWekesa@users.noreply.github.com>
2023-02-19 11:26:45 +0300

    f

--- app/client/jS/login-form.js
+++ app/client/jS/login-form.js
@@ -1,2 +1,20 @@
 console.log('hi')
+const eyeIcon = document.querySelectorAll('ion-icon');
+	passwordField = document.querySelectorAll(".password");
+	forms = document.querySelectorAll("form");
+	links = document.querySelectorAll(".link");
 
+
+	links.forEach(link => {
+		link.addEventListener("click", () =>{
+			forms.forEach(form => {
+				form.classList.toggle('display-none')
+			})
+		})
+	});
+
+forms.forEach(form => {
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+  });
+});
--- app/client/jS/script.js
+++ app/client/jS/script.js
@@ -1,1 +1,10 @@
-console.log("hi")
+console.log("hi");
+
+const searchBox = document.querySelector("#search");
+      searchIcon = document.querySelector(".search ion-icon");
+      form = document.querySelector("form");
+
+searchIcon.addEventListener("click", () =>{
+	searchBox.classList.toggle("hide");
+})
+form.preventDefault();
