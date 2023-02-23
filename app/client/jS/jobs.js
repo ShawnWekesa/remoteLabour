14b0e29d4df1044f96ff6961ac32e5dd5e34b581
shawnWekesa <102484330+ShawnWekesa@users.noreply.github.com>
2023-02-23 14:09:50 +0300

    w

--- app/client/jS/jobs.js
+++
@@ -1,6 +0,0 @@
-const searchBox = document.querySelector("#search");
-      searchIcon = document.querySelector(".search ion-icon");
-
-searchIcon.addEventListener("click", () =>{
-	searchBox.classList.toggle("hide");
-})
