6b3c4b7eb10788972530092db7e17f6ad973d06f
wechu07 <wechu017@gmail.com>
2023-03-06 19:52:58 +0300

    move static files to public folder

---
+++ public/jS/script.js
@@ -0,0 +1,18 @@
+console.log("hi");
+
+const searchBox = document.querySelector("#search");
+      searchIcon = document.querySelector(".search ion-icon");
+      form = document.querySelector("form");
+      nav = document.querySelector("nav");
+      divs = document.querySelectorAll("section > div");
+
+searchIcon.addEventListener("click", () =>{
+	searchBox.classList.toggle("hide");
+})
+
+divs.forEach(div => {
+  div.addEventListener('mouseenter', () => {
+    const backgroundColor = window.getComputedStyle(div).backgroundColor;
+    nav.style.backgroundColor = backgroundColor;
+  });
+});
